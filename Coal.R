6e141556f83eecf4d895bac19966567ac4130019
Al Kepner <albert.kepner@outlook.com>
2021-01-02 10:58:34 -0500

    Coal example with dplyr.

--- Coal.R
+++ Coal.R
@@ -2,10 +2,24 @@
 NEI <- readRDS("data/summarySCC_PM25.rds")
 SCC <- readRDS("data/Source_Classification_Code.rds")
 
-coal <- subset(SCC, EI.Sector == "Fuel Comb - Comm/Institutional - Coal")
+library(dplyr)
 
-coal_source <- as.character(coal$SCC)
+# find col combustion related SCC
+scc_coal <- SCC %>% select(SCC, EI.Sector) %>% filter(grepl("Coal",EI.Sector))
 
+#join tow table 
+coal <- merge(NEI, scc_coal, by= "SCC")
+
+#summary by year
+s <- with(coal, tapply(Emissions, as.character(year), sum))
+
+#create data frame and plot
+d <- data.frame(Year=names(s), CoalPM2.5 = s)
+
+plot(d$Year, d$CoalPM2.5, pch=19)
+
+
+## plot(as.numeric(d$Year), d$CoalPM2.5, pch=19)
 
 
 
